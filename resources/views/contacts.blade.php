2afd775823fbefad5e41fa55b68d42a980650342
Faza15 <3103120092@student.smktelkom-pwt.sch.id>
2022-03-06 22:46:37 +0700

    update modul 4 part2

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,18 +2,18 @@
 
 @section('container')
 <h1 class="mb-3">Contact Us</h1>
-    <form method="POST" >
+    <form method="POST" action="{{route('contacts.store')}}">
         @csrf
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Nama" name="nama">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Nama" name="Nama">
             <label for="floatingInput">Nama</label>
         </div>
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email">
+            <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="Email">
             <label for="floatingInput">Email</label>
         </div>
         <div class="form-floating mb-3">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="pesan"
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="Pesan"
                 placeholder="Tulis pesanmu disini" style="height: 100px"></textarea>
             <label for="floatingTextarea2">Pesan</label>
         </div>
